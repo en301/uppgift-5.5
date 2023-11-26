f40a03a5eecef93a78375beec4438693934308ee
en301 <emilsjostrand1000@gmail.com>
2023-11-26 12:07:38 +0100

    Add project files.

---
+++ uppgift 5.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace uppggift_5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            string fortsätt = "";
+            string[] svar = new string[10] {"Ja", "Nej", "Definitivt", "Definitivt inte", "Kanske", "Förmodligen", "Förmodligen inte", "Otroligt", "Potensiellt", "Möjligtvis"};
+            do
+            {
+                Console.WriteLine("Vill du ställa en fråga till och avsluta? Skriv då ja");
+                fortsätt = Console.ReadLine();
+                Console.WriteLine("Ställ din fråga");
+                string fråga = Console.ReadLine();
+                Console.WriteLine(svar[slump.Next(11)]);
+            } while (fortsätt != "ja");
+        }
+    }
+}
